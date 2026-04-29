1ba1e1bb0fcbd6756dfc320d9fdadf46de730470
sermoragon <sermoragon@gmail.com>
2026-04-29 12:36:37 +0100

    Cleaning project structure

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
 import { IssuerAModule } from './modules/issuer-a/issuer-a.module';
 import { IssuerBModule } from './modules/issuer-b/issuer-b.module';
 
 @Module({
   imports: [HealthModule, IssuerAModule, IssuerBModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
