8436c43c0823425562747f257bc64d9d4b6c4eef
sermoragon <sermoragon@gmail.com>
2026-04-29 12:08:27 +0100

    First restructuring and sorting the project modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
-import { PaymentsModule } from './modules/payments/payments.module';
 import { IssuerAModule } from './modules/issuer-a/issuer-a.module';
 import { IssuerBModule } from './modules/issuer-b/issuer-b.module';
-import { PersistenceModule } from './modules/persistence/persistence.module';
-import { ObservabilityModule } from './modules/observability/observability.module';
-import { Iso20022Module } from './modules/iso20022/iso20022.module';
-
 
 @Module({
-  imports: [HealthModule, PaymentsModule, IssuerAModule, IssuerBModule, PersistenceModule, ObservabilityModule, Iso20022Module],
+  imports: [HealthModule, IssuerAModule, IssuerBModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/modules/issuer-a/issuer-a.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('issuer-a')
-export class IssuerAController {}
